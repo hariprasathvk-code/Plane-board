420e4e75c298fda998a10e0d94e994432454724a
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-17 01:20:36 -0700

    First class and half first class commit

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,30 @@ using namespace std;
 
 // Priority Boarding Process
 void PriorityBoarding() {
-    
+    cout << "\n Priority Boarding Started \n";
+
+    while (true) {
+        bool firstClass = false;
+        cout << "Is there a First Class passenger? (1 = Yes, 0 = No): ";
+        cin >> firstClass;
+
+        if (firstClass) {
+            cout << "First class passenger boards the plane.\n";
+            cout << "Returning from Priority Boarding\n";
+            break; // return
+        } else {
+            bool halfFirstClass = false;
+            cout << "Has half of first class boarded? (1 = Yes, 0 = No): ";
+            cin >> halfFirstClass;
+
+            if (halfFirstClass) {
+                cout << "All passengers board the plane.\n";
+                cout << "Returning from Priority Boarding...\n";
+                break; // return
+            } else {
+                cout << "Restarting Priority Boarding process...\n";
+            }
+        }
     
 }
 
